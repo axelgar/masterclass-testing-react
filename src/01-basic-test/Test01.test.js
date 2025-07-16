2af5afb778108094602d5a4afe5cd926ff217d64
Àxel García <garciaiax@mediamarkt.es>
2025-07-16 13:33:29 +0200

    feat: :memo: Add more docs

--- src/01-basic-test/Test01.test.js
+++ src/01-basic-test/Test01.test.js
@@ -1,3 +1,4 @@
+import { test } from "vitest";
 import { getDiscountPercentage } from "../utils/getDiscountPercentage";
 
 /**
@@ -5,8 +6,10 @@ import { getDiscountPercentage } from "../utils/getDiscountPercentage";
  * Test should have as much as possible all the information they need to run. Not be afraid of repeated code.
  * Test should test one thing. A good rule of thumb is having one "expect" statement per test.
  * Every file should test one system (util, hook, component, etc.).
+ * Be consistent!
  * Think of them as a way to document the system. Good place to start for understanding new code.
  * Follow Arrange - Act - Assert structure.
+ * https://vitest.dev/api/#test-api-reference
  */
 
 describe(getDiscountPercentage, () => {
