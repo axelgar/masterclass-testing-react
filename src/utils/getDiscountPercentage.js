939d5434db932c6a1757be147dd949f6f416938c
Àxel García <garciaiax@mediamarkt.es>
2025-07-16 13:00:01 +0200

    fix: :bug: Fix early return value in discount util

--- src/utils/getDiscountPercentage.js
+++ src/utils/getDiscountPercentage.js
@@ -1,9 +1,9 @@
 export function getDiscountPercentage(product) {
   if (!product.price || !product.discountPrice) {
-    return;
+    return 0;
   }
   if (product.price === product.discountPrice) {
-    return;
+    return 0;
   }
 
   const discount = ((product.price - product.discountPrice) / product.price) * 100;
